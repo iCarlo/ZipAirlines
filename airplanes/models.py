5d870faf8e7eee22dd194fd000249ea85be65a51
iCarlo <62226010+iCarlo@users.noreply.github.com>
2020-08-09 12:28:52 +0800

    Airplane models created and migrated

--- airplanes/models.py
+++ airplanes/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Airplanes(models.Model):
+    model_id = models.IntegerField(max_length=10)
+    name = models.CharField(max_length=100)
+    number_of_passengers = models.IntegerField()
+    fuel_capacity = models.IntegerField()
+    total_fuel_consumption_per_minute = models.FloatField()
+    maximum_flight_time = models.FloatField()
+
